6f5aca22e0e03c1d8f59f97a079ea10179544f2c
James Benner <james.benner@gmail.com>
2017-02-15 20:32:33 -0500

    Implemented some basic output markup.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import Card = jf2.Card;
 
-function hello(card: Card): void {
-    console.log(card.name);
+function getStory(card: Card): string {
+    return `<article class="h-card">
+                <h1 class="p-name">${card.name}</h1>
+                <h2 class="p-note">${card.note}</h2>
+                <dl>
+                    <dt>Email:</dt>
+                    <dd><a class="u-email" href="mailto:${card.email}">${card.email}</a></dd>
+                    <dt>Tel:</dt>
+                    <dd><a class="p-tel" href="tel:+${card.tel}">${card.tel}</a></dd>
+                    <dt>URL:</dt>
+                    <dd><a class="u-url" href="${card.url}">${card.url}</a></dd>
+                </dl>
+            </article>`;
 }
 
-let card = {name: "James"};
+let card = {
+    name: "James Benner",
+    note: "Geek and Stuff",
+    email: "me@example.org",
+    tel: "KL5.867.5309",
+    url: "http://www.example.org"
+};
 
-hello(card);
+console.log(getStory(card));
